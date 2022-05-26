aecb0ba02c05d656e95178ca51edc37bc54deb17
beka <zumabaevbeka0@gmail.com>
2022-05-26 20:39:02 +0600

    post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
+      const posts = res.data;
+      this.setState({ posts });
+    });
+  }
+
+  deleteRow(id) {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+
+        const posts = this.state.posts.filter((item) => item.id !== id);
+        this.setState({ posts });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Post title</h1>
+
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.posts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td className="btn-td">
+                  <button
+                    className="btn btn-danger"
+                    onClick={(e) => this.deleteRow(post.id, e)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
